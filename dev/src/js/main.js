47af85fd9ec016245f76d51721b4e9b77e0acf66
Valerii Logvin <valerii.logvin@gmail.com>
2021-01-17 23:44:57 +0200

    sect1

---
+++ dev/src/js/main.js
@@ -0,0 +1,10 @@
+let $section1_slider_slidk = $('.js_slider');
+
+$section1_slider_slidk.slick({
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    arrows: false,
+    speed: 300,
+    slidesToShow: 1,
+});
